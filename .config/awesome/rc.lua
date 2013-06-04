548bcaef4e756a6d41cb610b952a69f383ab6417
Phil Pirozhkov <pirjsuka@gmail.com>
2013-06-04 18:59:09 +0400

    bring back horizontal split

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -60,7 +60,7 @@ local layouts =
     awful.layout.suit.tile,
 --    awful.layout.suit.tile.top,
 --    awful.layout.suit.tile.left,
---    awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.bottom,
 --    awful.layout.suit.fair,
 --    awful.layout.suit.fair.horizontal,
 --    awful.layout.suit.spiral,
