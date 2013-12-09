74c1f832c5a63822a06fef577d7f38b0f7ff30d8
Phil Pirozhkov <pirjsuka@gmail.com>
2013-12-09 18:40:34 +0700

    fix broken xsidle on additional arguments

--- .config/xsidle.sh
+++ .config/xsidle.sh
@@ -1,28 +1,26 @@
 #!/bin/sh
 #
 # Use xset s $time to control the timeout when this will run.
-#
 
 if [ $# -lt 1 ];
 then
-	printf "usage: %s cmd\n" "$(basename $0)" 2>&1
-	exit 1
+  printf "usage: %s cmd\n" "$(basename $0)" 2>&1
+  exit 1
 fi
-cmd="$1"
+cmd="$@"
 
 while true
 do
-	if [ $(xssstate -s) != "disabled" ];
-	then
-		tosleep=$(($(xssstate -t) / 1000))
-		if [ $tosleep -le 0 ];
-		then
-			$cmd
-		else
-			sleep $tosleep
-		fi
-	else
-		sleep 10
-	fi
+  if [ $(xssstate -s) != "disabled" ];
+  then
+    tosleep=$(($(xssstate -t) / 1000))
+    if [ $tosleep -le 0 ];
+    then
+      $cmd
+    else
+      sleep $tosleep
+    fi
+  else
+    sleep 10
+  fi
 done
-
