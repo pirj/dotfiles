d373347512c5dcdb6788a3c1b75f16162df44376
Phil Pirozhkov <pirjsuka@gmail.com>
2014-08-01 18:56:11 +0400

    oh-my-fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,27 +1,16 @@
-set __fish_git_prompt_showdirtystate 1
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
 
-# . ~/.config/fish/vi-mode.fish
-#
-# function fish_user_key_bindings
-#   vi_mode_insert
-# end
+# Theme
+set fish_theme syl20bnr
 
-# only show hostname (highlighted) if connected to remote
-function fish_prompt -d "Write out the prompt"
-  printf '%s@%s:%s%s%s%s%s%s $ ' (whoami) (hostname|cut -d . -f 1) (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (set_color $fish_color_quote) (__fish_git_prompt) (set_color normal)
-end
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set fish_plugins autojump bundler
+set fish_plugins sprunge vi-mode
 
-function fish_right_prompt -d "Write out the right prompt"
-  date "+[%H:%M:%S] "
-end
+# Path to your custom folder (default path is $FISH/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
 
-# SSH agent
-# when fish isn't a login shell, keychain passes wrong (sh/bash/zsh's) script for eval
-set SHELL fish; eval (keychain --eval --agents ssh,gpg --quiet .ssh/id_*a)
-
-# start X at login
-if status --is-login
-  if test -z "$DISPLAY" -a $XDG_VTNR = 1
-    exec startx
-  end
-end
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
