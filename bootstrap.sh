9d74c11ab150e416fd50a5ca03d16f234e6d7849
Phil Pirozhkov <pirjsuka@gmail.com>
2015-01-23 01:03:26 +0300

    submodularize!

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
 mkdir -p .local/vim/undo .local/vim/temp .local/vim/backup
 mkdir -p .config/vim/bundle
-git clone git@github.com:gmarik/Vundle.vim.git .config/vim/bundle/Vundle.vim
+
+git submodule update
