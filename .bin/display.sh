4e16eeb612ff1d8b8d563b697d593517e1b39a96
Phil Pirozhkov <pirjsuka@gmail.com>
2015-11-08 13:32:53 +0300

    [X] switch monitors quickly

--- .bin/display.sh
+++ .bin/display.sh
@@ -2,9 +2,9 @@
 
 VIDEO=`cat /tmp/video_out`
 if [[ "$VIDEO" == IN ]]; then
-    xrandr --output LVDS --off --output HDMI-0 --mode 1600x1200
+    xrandr --output LVDS-0 --off --output VGA-0 --preferred
     echo "EX" > /tmp/video_out
 else
-    xrandr --output LVDS --auto --output HDMI-0 --off
+    xrandr --output LVDS-0 --auto --output VGA-0 --auto
     echo "IN" > /tmp/video_out
 fi
