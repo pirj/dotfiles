c647bac4fd059cbed2337aab6233c0919ec92586
Phil Pirozhkov <hello@fili.pp.ru>
2024-02-25 16:52:11 +0300

    [vim] add telescope

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -10,5 +10,15 @@ lazy.setup({
       vim.o.timeoutlen = 500
     end,
     opts = { } -- KEEP THIS, otherwise it ceases to work
-  }
+  },
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-fzf-native.nvim' },
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+    end
+  },
 })
