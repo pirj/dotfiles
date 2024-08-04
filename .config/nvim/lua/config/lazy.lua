d2c5dffb6ae05fbf6fc94926bc794ca8e77c4725
Phil Pirozhkov <hello@fili.pp.ru>
2024-08-05 01:24:06 +0300

    collapse lines in nvim conf

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -1,11 +1,7 @@
 local lazy = require("lazy")
 
 lazy.setup({
-  {
-    "folke/which-key.nvim",
-    event = "VeryLazy",
-    config = require('config.which-key')
-  },
+  { "folke/which-key.nvim", event = "VeryLazy", config = require('config.which-key') },
   {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
@@ -16,20 +12,10 @@ lazy.setup({
       vim.keymap.set('n', '<leader>b', builtin.buffers, {})
     end
   },
-  {
-    'hrsh7th/nvim-cmp',
-    dependencies = { 'hrsh7th/cmp-buffer' },
-    config = require('config.cmp')
-  },
-  {
-    'numToStr/Comment.nvim',
-    lazy = false
-  },
+  { 'hrsh7th/nvim-cmp', dependencies = { 'hrsh7th/cmp-buffer' }, config = require('config.cmp') },
+  { 'numToStr/Comment.nvim', lazy = false },
   'whiteinge/diffconflicts',
   'tpope/vim-fugitive', -- mostly for blame
   'airblade/vim-gitgutter', -- show git changes
-  {
-    'David-Kunz/gen.nvim', -- Ollama local LLM gen AI
-    config = require('config.gen')
-  },
+  { 'David-Kunz/gen.nvim', config = require('config.gen') }, -- Ollama local LLM gen AI
 })
