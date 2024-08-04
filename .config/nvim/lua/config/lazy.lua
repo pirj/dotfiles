1c606521519f476a0f020219cb5b6c1ec3789bac
Phil Pirozhkov <hello@fili.pp.ru>
2024-08-05 02:07:47 +0300

    nvim: telescope -> fzf-lua

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -3,13 +3,15 @@ local lazy = require("lazy")
 lazy.setup({
   { "folke/which-key.nvim", event = "VeryLazy", config = require('config.which-key') },
   {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-fzf-native.nvim' },
+    "ibhagwan/fzf-lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons", -- optional for icon support
+      { "junegunn/fzf", build = "./install --bin" } -- not using it outside of vim
+    },
     config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+      local fzf = require("fzf-lua")
+      vim.keymap.set("n", "<leader>f", fzf.files)
+      vim.keymap.set("n", "<leader>b", fzf.buffers)
     end
   },
   { 'hrsh7th/nvim-cmp', dependencies = { 'hrsh7th/cmp-buffer' }, config = require('config.cmp') },
